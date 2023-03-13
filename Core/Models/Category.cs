87ca0464463ba9fe0da8fbb3e65c66497baae04e
Egeraldo Qejvani <egeraldoors@gmail.com>
2023-03-13 22:59:42 +0100

    Add sorting for category

--- Core/Models/Category.cs
+++ Core/Models/Category.cs
@@ -1,7 +1,15 @@
 ﻿namespace Core.Models;
 
-public class Category
+public class Category : IComparable<Category>
 {
     public byte Id { get; set; }
     public string Name { get; set; } = null!;
+
+	public int CompareTo(Category? other)
+	{
+		if (other == null)
+			return 1;
+
+		return this.Id.CompareTo(other.Id);
+	}
 }
